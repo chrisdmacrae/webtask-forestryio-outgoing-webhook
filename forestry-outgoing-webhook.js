2f0e5849f5469f26b7fe7d7d624b02d1a89c9d75
chrisdmacrae <hello@chrisdmacrae.com>
2018-02-23 16:04:02 -0400

    Updating forestry-outgoing-webhook.js

--- forestry-outgoing-webhook.js
+++ forestry-outgoing-webhook.js
@@ -1,11 +1,42 @@
-// Slack will send a request for each message sent on any or a specific channel.
-// If trigger word has been configured on Slack, only messages starting with
-// that trigger word will be sent
+// Forestry will send a request whenever a publish or import action completes,
+// https://forestry.io/docs/hosting/webhooks/#webhook-format
+var request = require("request-promise")
 
 /**
 * @param context {WebtaskContext}
+* @param cb {CompletionCallback}
 */
-module.exports = function (context, done) {
-  console.log('slack request: ', context.body);
-  done(null, { text: 'hello' });
+module.exports = function (context, cb) {
+  console.log('Forestry request: ', context.body);
+  
+  // Send outgoing webhook on Forestry action success
+  try {
+    if (context.body.success) {
+      var payload = {}
+      
+      // Send the outgoing Travis request; handle error from Travis
+      request({
+        method: 'POST',
+        uri: context.data.URI,
+        headers: {
+          // Any necessary headers
+        },
+        body: payload,
+        json: true
+      })
+        .then(function(res) {
+          cb(null, res)
+        })
+        .catch(function(err) {
+          cb(err)
+        })
+      
+    // Log the error message from Forestry on failure
+    } else {
+       cb(context.body.error); 
+    }
+  // Catch all stack errors
+  } catch(err) {
+    cb(err)
+  }
 };
